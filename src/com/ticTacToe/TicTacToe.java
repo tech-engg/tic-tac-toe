3bc36752af74c91adbec193ef894d298689485ed
tech-engg <techsolution.enggn@gmail.com>
2021-05-18 09:03:07 +0530

    create board

--- src/com/ticTacToe/TicTacToe.java
+++ src/com/ticTacToe/TicTacToe.java
@@ -4,8 +4,17 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		System.out.println("welocme to tic-tac-toe");
-		// TODO Auto-generated method stub
-
-	}
+		char[] board = createBoard();
+    }
+	public static char[] createBoard()
+	{
+		char[] board = new char[10];
+		for(int i=1; i < 10; i++)
+		{
+			board[i] =  ' ';
+			
+		}
+		return board;
+      }
 
 }
